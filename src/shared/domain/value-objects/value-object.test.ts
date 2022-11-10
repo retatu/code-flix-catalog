6020763fb5befcd38a81ac5814e966b6563d80ab
Matheus Rehbein <matheus.rehbein@traivefinance.com>
2022-11-10 08:41:19 -0300

    created tests for value object

--- src/shared/domain/value-objects/value-object.test.ts
+++ src/shared/domain/value-objects/value-object.test.ts
@@ -11,7 +11,24 @@ describe("ValueObject :: Unit Test", () => {
       expect(vo.value).toEqual("Fake value");
       vo = new StubValueObject({ prop1: 'Value1' });
       expect(vo.value).toStrictEqual({ prop1: 'Value1' });
-
+    })
+    test("The method toString should return the values correctly", () => {
+      const date = new Date();
+      const arrange = [
+        { received: { prop1: 'value' }, expected: JSON.stringify({ prop1: 'value' }) },
+        { received: date, expected: date.toString() },
+        { received: 'Fake value', expected: 'Fake value' },
+        { received: null, expected: 'null' },
+        { received: undefined, expected: 'undefined' },
+        { received: 5, expected: '5' },
+        { received: true, expected: 'true' },
+        { received: false, expected: 'false' },
+        { received: Number(7), expected: '7' },
+      ];
+      arrange.forEach((value) => {
+        let vo = new StubValueObject(value.received);
+        expect(`${vo}`).toEqual(value.expected);
+      })
     })
   });
 });
--- src/shared/domain/value-objects/value-object.ts
+++ src/shared/domain/value-objects/value-object.ts
@@ -7,4 +7,13 @@ export default abstract class ValueObject<Value = any>{
   get value() : Value{
     return this._value;
   }
+  toString = () => {
+    if (this._value === null || this._value === undefined) {
+      return ""+this._value;
+    } else if (typeof this._value === "object" && this._value.toString() === '[object Object]') {
+      return JSON.stringify(this._value);
+    }
+    
+    return this._value.toString();
+  };
 }
