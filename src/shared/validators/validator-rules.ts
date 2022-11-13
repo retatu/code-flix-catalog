6aa1938de1af5d485cc0022a502b8f68111ad71b
Matheus Rehbein <matheus.rehbein@traivefinance.com>
2022-11-13 14:29:06 -0300

    validation logic

--- src/shared/validators/validator-rules.ts
+++ src/shared/validators/validator-rules.ts
@@ -1,30 +1,41 @@
-import ValidationError from "shared/errors/validation.error";
+import ValidationError from "../errors/validation.error";
 
 export default class ValidatorRules {
-  private constructor(private value: any, private property: string) {}
-  
+  private constructor(private value: any, private property: string) { }
+
   static values(value: any, property: string): ValidatorRules {
     return new ValidatorRules(value, property);
   }
 
-  required(): this{
+  required(): Omit<this, 'required'> {
     if ([null, undefined, ''].includes(this.value)) {
       throw new ValidationError(`The ${this.property} is required`);
     }
     return this;
   }
 
-  string(): this{
-    if (typeof this.value !== "string") {
+  string(): Omit<this, 'string'> {
+    if (!isEmpty(this.value) && typeof this.value !== "string") {
       throw new ValidationError(`The ${this.property} is not a string`);
     }
     return this;
   }
 
-  maxLength(max: number): this{
-    if (this.value.length > max) {
+  boolean(): Omit<this, 'boolean'> {
+    if (!isEmpty(this.value) && typeof this.value !== "boolean") {
+      throw new ValidationError(`The ${this.property} is not a boolean`);
+    }
+    return this;
+  }
+
+  maxLength(max: number): Omit<this, 'maxLength'> {
+    if (!isEmpty(this.value) && this.value.length > max) {
       throw new ValidationError(`The ${this.property} must be less or equal than ${max} characters`);
     }
     return this;
   }
+}
+
+export function isEmpty(value: any) {
+  return value === undefined || value === null;
 }
